3ae0363447c4a085f3ee41e621f516a7074a4e88
huaxing-w <hwang68@syr.edu>
2022-01-05 22:23:45 -0800

    1st

---
+++ LC/DP/Best Time to Buy and Sell Stock/714.Best Time to Buy and Sell Stock with Transaction Fee/121. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_=prices[0]
+        ans=0
+        for i in prices:
+            ans=max(ans,i-min_)
+            min_=min(min_,i)
+        return ans
