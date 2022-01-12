25998e3aef86b85b90243132821a2d8d88a8de73
huaxing-w <90473380+huaxing-w@users.noreply.github.com>
2022-01-12 08:47:46 -0800

    Create dp.py

---
+++ LC/DP/double string dp/115. Distinct Subsequences/dp.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m=len(s)
+        n=len(t)
+
+        s='#'+s
+        t='#'+t
+
+        dp=[[0]*(n+1) for i in range(m+1)]
+        dp[0][0]=1
+        for i in range(1,m+1):
+            dp[i][0]=1
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i]==t[j]:
+                    dp[i][j]+=dp[i-1][j-1]
+                dp[i][j]+=dp[i-1][j]
+        return dp[m][n]
